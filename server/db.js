103d13d37683847ded0eec71f95a823b25dabaea
Ayush Mishra <work.ayushhh@gmail.com>
2026-03-29 16:30:36 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,17 +1,5 @@
-require('dotenv').config();
-
-const { Pool } = require('pg');
-
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: '/var/run/postgresql',
   database: 'trello_clone',
-  password: 'postgres',
-  port: 5432,
 });
-
-pool.connect()
-  .then(() => console.log('✅ Connected to PostgreSQL'))
-  .catch(err => console.error('❌ DB Connection Error:', err));
-
-module.exports = pool;
